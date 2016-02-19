cf77c1a2afe56c2958f74a2ace0cc468da52f4a5
Nirbheek Chauhan <nirbheek.chauhan@gmail.com>
2016-02-20 00:40:35 +0530

    meson: Fix pbutils-enumtypes.c generation
    Due to a typo, the contents of the .c file were identical to the .h file

--- gst-libs/gst/pbutils/pbutils_mkenum.py
+++ gst-libs/gst/pbutils/pbutils_mkenum.py
@@ -18,13 +18,15 @@ h_array = ['--fhead',
 ]
 
 c_array = ['--fhead',
-           "#ifndef __PB_UTILS_ENUM_TYPES_H__\n#define __PB_UTILS_ENUM_TYPES_H__\n\n#include <glib-object.h>\n\nG_BEGIN_DECLS\n",
+           "#include \"pbutils-enumtypes.h\"\n\n#include \"pbutils.h\" \n#include \"codec-utils.h\" \n#include \"descriptions.h\" \n#include \"encoding-profile.h\" \n#include \"encoding-target.h\" \n#include \"install-plugins.h\" \n#include \"missing-plugins.h\" \n#include \"gstdiscoverer.h\" \n#include \"gstaudiovisualizer.h\"",
            '--fprod',
-           "\n/* enumerations from \"@filename@\" */\n",
+           "\n/* enumerations from \"@filename@\" */",
            '--vhead',
-           "GType @enum_name@_get_type (void);\n#define GST_TYPE_@ENUMSHORT@ (@enum_name@_get_type())\n",
-           '--ftail',
-           "G_END_DECLS\n\n#endif /* __PB_UTILS_ENUM_TYPES_H__ */"
+           "GType\n@enum_name@_get_type (void)\n{\n  static volatile gsize g_define_type_id__volatile = 0;\n  if (g_once_init_enter (&g_define_type_id__volatile)) {\n    static const G@Type@Value values[] = {",
+           '--vprod',
+           "      { @VALUENAME@, \"@VALUENAME@\", \"@valuenick@\" },",
+           '--vtail',
+           "      { 0, NULL, NULL }\n    };\n    GType g_define_type_id = g_@type@_register_static (\"@EnumName@\", values);\n    g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);\n  }\n  return g_define_type_id__volatile;\n}\n"
 ]
 
 ofilename = sys.argv[1]
